590398e498f893fbcdb9d277fca95db75a95fc12
ANDALOS <Taher226>
2024-03-13 15:17:24 +0200

    initial project. added empty home screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:untitled4/counter/counter_screen.dart';
-import 'package:untitled4/testing/test_screen.dart';
+import 'package:untitled4/islami/home_screen.dart';
 
 void main() {
-  runApp( MyApp() );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-
   // This widget is the root of your application.
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-    home: testScreen(),
-    /*initialRoute: GameBoard.routeName,
+      initialRoute: HomeScreen.routeName,
       routes: {
-        CounterScreen.routeName : (context) => CounterScreen(),
-        GameBoard.routeName : (context) => GameBoard(),
-        GameBoard.routeName : (context) => GameBoard(),
-
-      },*/
+        HomeScreen.routeName: (context) => HomeScreen(),
+        /*GameBoard.routeName : (context) => GameBoard(),
+        GameBoard.routeName : (context) => GameBoard(),*/
+      },
     );
   }
 }
